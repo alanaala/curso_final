1af49827c551a64ef3a8fa9f667bb20eedf029aa
1025170 <alanamf07@gmail.com>
2025-06-13 16:15:33 -0300

    Splash finalizado

--- app/src/main/java/com/alana/applistacursos/view/SplashActivity.java
+++ app/src/main/java/com/alana/applistacursos/view/SplashActivity.java
@@ -25,6 +25,7 @@ public class SplashActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
         comutartelaSplash();
     }
 
