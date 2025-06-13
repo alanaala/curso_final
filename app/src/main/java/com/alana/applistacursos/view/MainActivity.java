4562f2079eb3603687d24797f487ba95054ee418
1025170 <alanamf07@gmail.com>
2025-06-13 15:56:20 -0300

    linear finalizado

--- app/src/main/java/com/alana/applistacursos/view/MainActivity.java
+++ app/src/main/java/com/alana/applistacursos/view/MainActivity.java
@@ -9,10 +9,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.alana.applistacursos.R;
 import com.alana.applistacursos.controller.CursoController;
 import com.alana.applistacursos.controller.PessoaController;
